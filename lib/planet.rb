1316b090e9d61326d6b407897c09262005255a51
Garrett LeSage <garrett@lesage.us>
2015-02-16 16:33:26 +0100

    reworked the code a little, based on rubocop suggestions

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,38 +1,36 @@
 require 'nokogiri'
 require 'open-uri/cached'
 
-def rewrite_uris html, base
-  begin
-    doc = Nokogiri::HTML(html)
-    base_uri = URI.parse(base)
+def rewrite_uris(html, base)
+  doc = Nokogiri::HTML(html)
+  base_uri = URI.parse(base)
 
-    tags = {
-      a: 'href',
-      img: 'src',
-      script: 'src'
-    }
+  tags = {
+    a: 'href',
+    img: 'src',
+    script: 'src'
+  }
 
-    doc.search(tags.keys.join(',')).each do |node|
-      href = tags[node.name.to_sym]
-      orig_url = node[href]
-      uri = URI.parse(orig_url)
+  doc.search(tags.keys.join(',')).each do |node|
+    href     = tags[node.name.to_sym]
+    orig_url = node[href]
+    uri      = URI.parse(orig_url)
 
-      unless uri.host
-        node[href] = if uri.path.to_s.match(/^\//)
-          "#{base_uri.scheme}://#{base_uri.host}#{orig_url}"
-        else
-          base.gsub(/\/$/, '') + '/' + orig_url
-        end
-      end
+    unless uri.host
+      node[href] = if uri.path.to_s.match(/^\//)
+                     "#{base_uri.scheme}://#{base_uri.host}#{orig_url}"
+                   else
+                     base.gsub(/\/$/, '') + '/' + orig_url
+                   end
     end
-
-    return doc.to_html
-  rescue
-    puts "Warning: Issues rewriting URIs (content may still work): #{base}"
-    return html
   end
-end
 
+  return doc.to_html
+
+rescue
+  puts "Warning: Issues rewriting URIs (content may still work): #{base}"
+  return html
+end
 
 def planet_feeds
   return $PLANET_FEEDS if $PLANET_FEEDS
@@ -54,14 +52,14 @@ def planet_feeds
 
         all_feed_entries += entries.each do |item|
           # Add feed's title & url to each item
-          #item[:feed_title] = feed.title
-          item[:feed_name] = name
-          item[:feed_title] = feed.title || name
-          item[:feed_url] = feed.url
-          item[:feed_source] = feed_url
-          item[:feed_image] = info[:image]
+          item[:feed_name]     = name
+          item[:feed_title]    = feed.title || name
+          item[:feed_url]      = feed.url
+          item[:feed_source]   = feed_url
+          item[:feed_image]    = info[:image]
           item[:image_rounded] = info[:rounded]
         end
+
       rescue
         puts "Error loading #{feed_url}"
       end
@@ -69,5 +67,9 @@ def planet_feeds
 
   end
 
-  $PLANET_FEEDS = all_feed_entries.reject {|e| e.published.nil? || e.id.match(/archives/)}.sort_by {|e| e.published}.uniq {|e| e.url}.reverse
+  $PLANET_FEEDS = all_feed_entries
+                  .reject  { |e| e.published.nil? || e.id.match(/archives/) }
+                  .sort_by { |e| e.published }
+                  .uniq    { |e| e.url }
+                  .reverse
 end
